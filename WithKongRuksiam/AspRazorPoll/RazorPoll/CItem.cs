d7c8913d6b8e306d746fd04b47441c3f2e9bc36f
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-05-05 07:41:04 +0700

    Razor Poll Complete
    Pj Razor Poll is Complete

---
+++ WithKongRuksiam/AspRazorPoll/RazorPoll/CItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazorPoll
+{
+    public class CItem
+    {
+        /* บันทึกการแก้ปัญหา 2020/04/05
+         * key  ต้องเป็นตัวพิมพ์เล็ก ตาม คู่มือการใช้งาน
+         *  https://morrisjs.github.io/morris.js/donuts.html 
+         * วนตรงนี้เป็นวัน หาสาเหตุ debug ดูหลายๆ ตัว ทั้งเปิด pj ต้นแบบของคุณ kong และ pj ของเรา จนพบว่า คู่มือของ morris ระบุว่าต้องใช้ ตัวพิมพ์เล็กเป็น key
+         * และ JsonConvert.SerializeObject จะเอาไป convert เป็น key และ value
+        */
+
+        public String label { get; set; }
+        public String value { get; set; }
+    }
+}
---
+++ WithKongRuksiam/AspRazorPoll/RazorPoll/RunTest/ResponseWrite.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" Debug="true" %>
+<script runat="server">
+    void Page_Load(object sender, EventArgs e)		     
+	{
+		this.lblText.Text = " maintain Text From Label";
+		Response.Write("Write Text From Response.Write");
+	}
+</script>
+<html>
+<head>
+<title>Testing Response Object</title>
+</head>
+<body>
+	<form id="form1" runat="server">	
+	<asp:Label id="lblText" runat="server"></asp:Label>
+	</form>
+</body>
+</html>
